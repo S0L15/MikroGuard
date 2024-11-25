92a338383ba958e3bfa827e40a5f219e35e2b46f
S0L15 <felipesolisrpo@gmail.com>
2024-11-25 13:21:21 -0500

    x

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    name="MikroGuard",
+    version="1.0.0",
+    author="S0L15",
+    author_email="d3v.s0l15@gmail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/S0L15/MikroGuard.git"
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.12',
 )
